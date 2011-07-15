6ee1c6d9eb08915391fdda4b46c8dfa650d741a8
Stanford Rosenthal <nolastan@gmail.com>
2011-07-15 15:56:41 -0700

    Added CSS and Google Map

--- application/views/header.php
+++ application/views/header.php
@@ -1,15 +1,20 @@
 <?=doctype()?>
 <html>
 <head>
-<?=link_tag('css/style.css')?>
+<?=link_tag('css/reset.css')?>
+<?=link_tag('css/one-page.css')?>
+<?=link_tag('css/custom.css')?>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 <script src="<?=site_url('js/script.js')?>" type="text/javascript"></script>
+<script src="<?=site_url('js/map.js')?>" type="text/javascript"></script>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 <title>Open311</title>
 </head>
-<body>
-<div id="wrapper">
-	<header>
-		<h1>Open311</h1>
-	</header>
-	<div id="content">
+<body onload="initialize()">
+<div id="content">
+<header class="clearfix">
+	<h1 id="project_name"><?=$settings['project_name']?></h1>
+	<h2 id="company_name"><?=$settings['company_name']?></h2>
+</header>
--- application/views/web/home.php
+++ application/views/web/home.php
@@ -1,43 +1,65 @@
-<?=form_open('home/submit')?>
-	<?=form_hidden('jurisdiction_id', 'sfgov.org')?>
+<h3 id="project_description"><?=$settings['project_description']?></h3>
 
-	<div id="group">
-		<label for="group">Category</label>
-		<select name="group">
-			<option value="null">-- Select a Category --</option>
-		<? foreach($group_names as $group_name): ?>
-			<option value="<?=str_replace(" ","_",$group_name)?>"><?=$group_name?></option>
-		<? endforeach ?>
-		</select>
-	</div>
-	<div id="service">
+
+<?=form_open_multipart('home/submit')?>
+	<?=form_hidden('jurisdiction_id', 'sfgov.org')?>
+	<fieldset id="service_selection">
+		<div id="group">
+			<label for="group">Category</label>
+			<select name="group">
+				<option value="null">-- Select a Category --</option>
+			<? foreach($group_names as $group_name): ?>
+				<option value="<?=str_replace(" ","_",$group_name)?>"><?=$group_name?></option>
+			<? endforeach ?>
+			</select>
+		</div>
+		<div id="service">
+		
+		</div>
+	</fieldset>
+	<fieldset id="meta">
+		<div class="field">
+			<?=form_label('Details','description')?>
+			<?=form_textarea('description')?>
+		</div>
+		<div class="field">
+			<?=form_label('Photo','fileupload')?> 
+			<?=form_upload('fileupload')?> <!-- media_url -->
+		</div>
+	</fieldset>
+	<fieldset id="location">
+		<div class="address field">
+			<?=form_label('Address','address_string')?>
+			<?=form_input('address_string', null, 'id="address_string"')?>		
+			<div class="map_help">Click the map or enter an address.</div>
+		</div>
+		<div id="map_canvas"></div>
+		<?=form_hidden('lat', null, 'id="lat"')?>
+		<?=form_hidden('long', null, 'id="long"')?>
+	</fieldset>
+
+	<fieldset id="contact">
+		<div class="field">
+			<?=form_label('Email Address','email')?>
+			<?=form_input('email')?>
+		</div>
+		<div class="field">
+			<?=form_label('First','first_name')?>
+			<?=form_input('first_name')?>
+		</div>
+		<div class="field">
+			<?=form_label('Last','last_name')?>
+			<?=form_input('last_name')?>
+		</div>
+		<div class="field">
+			<?=form_label('Phone Number','phone')?>
+			<?=form_input('phone')?>
+		</div>
+	</fieldset>
 	
-	</div>
-	<div id="meta">
-		<?=form_label('Details','description')?>
-		<?=form_textarea('description')?>
-		<?=form_label('Photo','media')?> <!-- media_url -->
-		<?=form_upload('media')?>
-	</div>
-	<div id="location">
-		<?=form_label('Latitude','lat')?>
-		<?=form_input('lat')?>
-		<?=form_label('Longitude','long')?>
-		<?=form_input('long')?>
-		<?=form_label('Address','address_string')?>
-		<?=form_input('address_string')?>		
-	</div>
-	<div id="contact">
-		<?=form_label('Email Address','email')?>
-		<?=form_input('email')?>
-		<?=form_label('First','first_name')?>
-		<?=form_input('first_name')?>
-		<?=form_label('Last','last_name')?>
-		<?=form_input('last_name')?>
-		<?=form_label('Phone Number','phone')?>
-		<?=form_input('phone')?>
-	</div>
+	<a class="large green button" id="submit">Submit</a>
 	
-	<?=form_submit('submit','Submit')?>
 	
 <?=form_close()?>
+
+
