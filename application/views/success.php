3be3fc7668a3a2861426fa82b530f16a6224936d
Stanford Rosenthal <nolastan@gmail.com>
2011-08-05 10:28:56 -0700

    Moved SUCCESS MESSAGE in case no service_request_id is available

--- application/views/success.php
+++ application/views/success.php
@@ -1,6 +1,7 @@
 <?=$this->load->view('header')?>
 <h1>Thank you for your submission.</h1>
 <p><?=$response->request->service_notice?></p>
-<p>Your service request ID is <?=$response->request->service_request_id?>. <?=SUCCESS_MESSAGE?></p>
+<? if($response->request->service_request_id): ?><p>Your service request ID is <?=$response->request->service_request_id?>.</p><? endif; ?>
+<p><?=SUCCESS_MESSAGE?></p>
 <p><?=anchor($this->session->userdata('medium'), 'Back')?></p>
 <?=$this->load->view('footer')?>
